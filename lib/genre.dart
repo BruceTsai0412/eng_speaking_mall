534d4453025c4a83ee4513e43e312bee1cab29f7
Bruce Tsai <brucetsai0412@gmail.com>
2024-03-19 01:34:27 +0800

    update level

---
+++ lib/genre.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'genre_1_romance.dart';
+
+class Genre extends StatelessWidget {
+  final List<String> genreNames = [
+    'Romance',
+    'Sci-Fi',
+    'Comedy',
+    'Adventure',
+  ];
+
+  final List<String> imagePaths = [
+    'image/pos_la_la_land.png',
+    'image/pos_back_to_the_future.png',
+    'image/pos_me_time.png',
+    'image/pos_jumanji.png',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Genre'),
+      ),
+      backgroundColor: Color(0xffffcd45),
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox.shrink(),
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Align(
+                  alignment: Alignment(-0.9, 0),
+                  child: Text('Select a genre', style: const TextStyle(fontSize: 20.0),),
+                ),
+              ),
+              ...List.generate(
+                4,
+                (index) => Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xfffcf3e3),
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 1.5,
+                    ),
+                  ),
+                  padding: const EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 5.0),
+                  margin: const EdgeInsets.all(5.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              genreNames[index],
+                              style: const TextStyle(fontSize: 20.0, color: Colors.black),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 100,
+                              height: 150,
+                              margin: EdgeInsets.fromLTRB(10.0, 0.0, 35.0, 0.0),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                image: DecorationImage(
+                                  fit: BoxFit.fill,
+                                  image: AssetImage(imagePaths[index]),
+                                ),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.5),
+                                    spreadRadius: 5,
+                                    blurRadius: 7,
+                                    offset: Offset(0, 3), // changes position of shadow
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Transform.translate(
+                              offset: Offset(0.0, 50.0),
+                              child: SizedBox(
+                                width: 100,
+                                height: 40,
+                                child: Container(
+                                  margin: EdgeInsets.all(1.0),
+                                  child: ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(builder: (context) => Genre1()),
+                                      );
+                                    },
+                                    style: ElevatedButton.styleFrom(backgroundColor: Color(0xFF264683)),
+                                    child: Text('Enter', style: const TextStyle(color: Color(0xffffffff),),),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'genre_1_romance.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Speaking Learning App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -21,25 +18,19 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<String> _genreNames = [
-    'Romance',
-    'Sci-Fi',
-    'Comedy',
-    'Adventure',
-  ];
-
-    final List<String> _imagePaths = [
-    'image/pos_la_la_land.png',
-    'image/pos_back_to_the_future.png',
-    'image/pos_me_time.png',
-    'image/pos_jumanji.png',
+  final List<String> _levels = [
+    'Easy',
+    'Intermediate',
+    'Hard',
+    'Phonetics 1',
+    'Phonetics 2',
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('home'),
+        title: Text('Home'),
       ),
       backgroundColor: Color(0xffffcd45),
       body: Scrollbar(
@@ -66,85 +57,40 @@ class MyHomePage extends StatelessWidget {
                       padding: const EdgeInsets.all(5.0),
                       child: Align(
                         alignment: Alignment(-0.9, 0),
-                        child: Text('Select a genre', style: const TextStyle(fontSize: 20.0),),
-                      ),
+                        child: Text('Select a level', style: const TextStyle(fontSize: 20.0),),),
                     ),
                     ...List.generate(
-                      4,
-                      (index) => Container(
-                        decoration: BoxDecoration(
-                          color: const Color(0xfffcf3e3),
-                          borderRadius: BorderRadius.circular(20),
-                          border: Border.all(
-                            color: Colors.black,
-                            width: 1.5,
+                      5,
+                      (index) => InkWell(
+                        onTap:(){
+                          
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color(0xfffcf3e3),
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(
+                              color: Colors.black,
+                              width: 1.5,
+                            ),
                           ),
-                        ),
-                        padding: const EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 5.0),
-                        margin: const EdgeInsets.all(5.0),
-                        child: Row(
-                          children: [
-                            Expanded(
-                              flex: 1,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    _genreNames[index],
-                                    style: const TextStyle(fontSize: 20.0, color: Colors.black),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Expanded(
-                              flex: 2,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                 Container(
-                                    width: 100,
-                                    height: 150,
-                                    margin: EdgeInsets.fromLTRB(10.0, 0.0, 35.0, 0.0),
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(15),
-                                      image: DecorationImage(
-                                        fit: BoxFit.fill,
-                                        image: AssetImage(_imagePaths[index],),
-                                      ),
-                                      boxShadow: [
-                                        BoxShadow(
-                                          color: Colors.grey.withOpacity(0.5),
-                                          spreadRadius: 5,
-                                          blurRadius: 7,
-                                          offset: Offset(0, 3), // changes position of shadow
-                                        ),
-                                      ],
+                          padding: const EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 5.0),
+                          margin: const EdgeInsets.all(5.0),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      _levels[index],
+                                      style: const TextStyle(fontSize: 20.0, color: Colors.black),
                                     ),
-                                  ),
-                                  Transform.translate(
-                                    offset: Offset(0.0, 50.0),
-                                    child: SizedBox(
-                                      width: 100,
-                                      height: 40,
-                                      child: Container(
-                                        margin: EdgeInsets.all(1.0),
-                                        child: ElevatedButton(
-                                          onPressed: () {
-                                            Navigator.push(
-                                              context,
-                                              MaterialPageRoute(builder: (context) => Genre1()),
-                                            );
-                                          },
-                                          style: ElevatedButton.styleFrom(backgroundColor: Color(0xFF264683)),
-                                          child: Text('Enter', style: const TextStyle(color: Color(0xffffffff),),),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
                     ),
